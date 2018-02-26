75e6e7fdc94492120e3436116e34513e38306713
Stanislav Protasov <stanislav.protasov@gmail.com>
2018-02-26 19:32:31 +0300

    buttons

---
+++ buttons.py
@@ -0,0 +1,22 @@
+import telegram
+
+START_BUTTONS = None
+
+
+def __init__():
+    global START_BUTTONS
+    START_BUTTONS = [[
+        telegram.KeyboardButton(text="/👩🍔"),
+        telegram.KeyboardButton(text="/🚼🍔"),
+        telegram.KeyboardButton(text="/🚼рост"),
+        telegram.KeyboardButton(text="/🚼вес"),
+    ],[
+        telegram.KeyboardButton(text='/🚼🤒'),
+        telegram.KeyboardButton(text='/🚼💊'),
+        telegram.KeyboardButton(text='/👩🤒'),
+        telegram.KeyboardButton(text='/👩💊'),
+    ],[
+        telegram.KeyboardButton(text="/🚼😴"),
+        telegram.KeyboardButton(text="/🚼😜"),
+        # telegram.KeyboardButton(text="/🌏", request_location=True),
+    ]]
